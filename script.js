5516add86b2cc8279965aab3345cc37d0f37f84a
Cristina.Chifor <cristinachifor93.cc@gmail.com>
2024-08-30 22:49:28 +0300

    Adds asybc examples

--- script.js
+++ script.js
@@ -1,1 +1,57 @@
 console.log("Hello!");
+
+// const promiseHandler = (resolve, reject) => {
+//     // fac ceva
+// }
+
+// const promise = new Promise(promiseHandler);
+
+const promise = new Promise((resolve, reject) => {
+    const resolved = false; // schimba in true sau false
+
+    setTimeout(() => {
+
+        if(resolved) {
+            resolve("Valoare dupa rezolvare");
+        } else {
+            reject("Valoare dupa respingere");
+        }
+    }, 2000);
+
+});
+
+promise.then((resolvedValue) => {
+    console.log(`S-a rezolvat cu valoarea: `, resolvedValue);
+
+}).catch((rejectedValue) => {
+    console.log(`S-a respins cu valoarea: `, rejectedValue);  
+
+}).finally(() => {
+    console.log("Se executa mereu");
+});
+
+console.log("Cod ce se executa simultan cu promisiunea");
+
+/** async/await este o functionalitate in JavaScript care simplifica gestionarea codului asincron 
+ * si face ca acesta sa para mai asemanator cu codul sincron. 
+ 
+ Folosind cuvAntul-cheie async In faTa unei funcTii, aceasta devine o funcTie asincron. 
+ Functiile asincrone returneaza intotdeauna o promisiune.
+ */
+
+ async function getUsers() {
+    setTimeout(() => {
+        return [
+            {username: "username1", password: "pass1"},
+            {username: "username2", password: "pass2"},
+
+        ]
+    }, 3000);
+ }
+
+ const users = getUsers();
+//  console.log(users);
+users.then((users) => {
+    console.log(users);
+}).catch((e) => console.log(e));
+
